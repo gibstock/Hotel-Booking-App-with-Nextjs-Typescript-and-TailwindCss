c51c82c92b5603f89bb143650cec90777f1e6e36
B-00 <107743153+B-00@users.noreply.github.com>
2022-09-26 22:19:28 -0400

    Spelling Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import ExploreTheWorld from '../components/ExploreTheWolrd'
+import ExploreTheWorld from '../components/ExploreTheWorld'
 import FeaturedDestinations from '../components/featured-destination/FeaturedDestinations'
 import Header from '../components/Header'
 import TrendingCities from '../components/trending-cities/TrendingCities'
