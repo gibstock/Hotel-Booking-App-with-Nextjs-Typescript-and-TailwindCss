5ca26166ef30cefe1008efea485ca53b07caa008
B-00 <107743153+B-00@users.noreply.github.com>
2022-09-27 00:58:58 -0400

    Added new sections

---
+++ components/destination-card/DestinationCard.tsx
@@ -0,0 +1,22 @@
+import CardTemplate from "./CardTemplate"
+
+export default function DestinationCard() {
+  return (
+    <div className="mt-[8rem]">
+      <h1 className="font-bold text-5xl text-center">Search a best place in the world</h1>
+      <p className="mt-6 text-center text-gray-400">Whether you’re looking for places for a vacation. We are here to Guide you
+      about the details you need to check in and ease your trips in advance</p>
+      <div className="px-[9rem] mt-[6rem] text-center grid grid-cols-4 gap-7 w-full">
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Batu, East Java'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Kuta' classes="bg-white-500 shadow-xl border-[0px]"/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Surabaya, East Java'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Malang, East Java'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Doemg, Central Java'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Nusa Dua, Lombok'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Bandung,West Java'/>
+        <CardTemplate img='/beach-palm-blue-sky-infinity.png' name='Watatobi,Sumatera'/>
+      </div>
+    </div>
+  )
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,13 @@ import FeaturedDestinations from '../components/featured-destination/FeaturedDes
 import Header from '../components/Header'
 import TrendingCities from '../components/trending-cities/TrendingCities'
 import Footer from '../components/Footer'
+import DestinationCard from '../components/destination-card/DestinationCard'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
+      <DestinationCard />
       <FeaturedDestinations />
       <ExploreTheWorld />
       <TrendingCities />
